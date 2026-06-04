e1b89a372cc7dc50ed76e9fe86dc44c1e0c7ee4f
Adrien Vigue <adrienvigue@gmail.com>
2026-06-04 13:20:40 +0200

    Suppression persistence BookingOption

--- src/main/java/fr/adriencaubel/hotel/adapter/persistence/repository/SpringDataBookingOptionRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.adriencaubel.hotel.adapter.persistence.repository;
-
-import fr.adriencaubel.hotel.adapter.persistence.entity.BookingOptionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpringDataBookingOptionRepository extends JpaRepository<BookingOptionEntity, Long> {
-}
